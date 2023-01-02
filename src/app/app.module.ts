fd4450d00860c6969ca8eae5cbbf293bdb2e96c6
Rakhmonov <get_info@bk.ru>
2023-01-02 07:53:52 +0500

    Install and integrate firebase packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { environment } from "../environments/environment";
+import { AngularFireDatabaseModule } from "@angular/fire/compat/database";
+import { AngularFireModule } from "@angular/fire/compat";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
