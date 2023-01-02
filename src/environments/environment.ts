1cd9a9c5a529b05415859f8e90339c98d97124e8
Rakhmonov <get_info@bk.ru>
2023-01-02 07:48:35 +0500

    Set firebase properties

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBMy5ul7w2OdDc1Bgv_aTZh71_lTfS9kvQ",
+    authDomain: "habitstracker-10036.firebaseapp.com",
+    databaseURL: "https://habitstracker-10036-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "habitstracker-10036",
+    storageBucket: "habitstracker-10036.appspot.com",
+    messagingSenderId: "1010700562331",
+    appId: "1:1010700562331:web:d49ae1e9a28a79e9fdfb3e",
+    measurementId: "G-VE8MYLFDLW"
+  },
 };
 
 /*
