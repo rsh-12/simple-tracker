4c603c36fc5c39e046b2fa97ef1f4436a29b0eb8
Rakhmonov <get_info@bk.ru>
2023-01-10 11:57:35 +0500

    Update mock habits title and description

--- src/app/services/mock-habit.service.ts
+++ src/app/services/mock-habit.service.ts
@@ -4,20 +4,20 @@ import { Habit } from "../models/habit";
 const mockHabits: Habit[] = [
   {
     key: '1',
-    title: 'Workout',
-    description: 'Do something',
+    title: 'Lorem ipsum dolor.',
+    description: 'Lorem ipsum dolor sit amet.',
     markedDays: new Set<string>(['2023-01-01', '2023-01-03', '2023-01-06'])
   },
   {
     key: '2',
-    title: 'Read books',
-    description: 'I want to read more and more books every fucking day',
+    title: 'Lorem.',
+    description: 'Lorem ipsum dolor sit amet, consectetur.',
     markedDays: new Set<string>(['2022-12-31', '2023-01-02', '2023-01-07'])
   },
   {
     key: '3',
-    title: 'Complete some tasks',
-    description: 'I don"t want to get fired',
+    title: 'Lorem ipsum.',
+    description: 'Lorem ipsum dolor.',
     markedDays: new Set<string>(['2022-12-01', '2023-01-05', '2023-01-10'])
   }
 ]
