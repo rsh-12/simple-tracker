20688ea63f5b633140daa6f4df1b3e51b66693f5
Rakhmonov <get_info@bk.ru>
2023-01-10 11:47:13 +0500

    Install and integrate Angular Material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,25 @@ import { AppComponent } from './app.component';
 import { environment } from "../environments/environment";
 import { AngularFireDatabaseModule } from "@angular/fire/compat/database";
 import { AngularFireModule } from "@angular/fire/compat";
+import { HabitsListComponent } from './components/habits-list/habits-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { HabitComponent } from './components/habit/habit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HabitsListComponent,
+    HabitComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
