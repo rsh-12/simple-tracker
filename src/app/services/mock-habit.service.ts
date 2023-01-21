b7b57606546ca4667d9583276c01c13268a7c7ae
Rakhmonov <get_info@bk.ru>
2023-01-21 08:47:55 +0500

    Add more habits

--- src/app/services/mock-habit.service.ts
+++ src/app/services/mock-habit.service.ts
@@ -19,14 +19,29 @@ const mockHabits: Habit[] = [
     title: 'Lorem ipsum.',
     description: 'Lorem ipsum dolor.',
     markedDays: new Set<string>(['2022-12-01', '2023-01-05', '2023-01-10'])
-  }
+  },
+  {
+    key: '4',
+    title: 'Lorem ipsum. Dolor color.',
+    description: 'Lorem ipsum dolor. Dolor.',
+    markedDays: new Set<string>(['2022-12-09', '2023-01-15', '2023-01-21'])
+  },
+  {
+    key: '5',
+    title: 'Lorem ipsum dolor sit amet.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquam, asperiores assumenda dolorum ' +
+      'enim ex excepturi expedita illum in iste iure maiores molestiae omnis pariatur placeat praesentium quaerat ' +
+      'quidem quos recusandae rem sequi sit tempore temporibus vel veniam veritatis vitae, voluptatem. ' +
+      'Ex magnam magni odit possimus repudiandae, similique tempora ut?',
+    markedDays: new Set<string>(['2023-01-20', '2023-01-19', '2023-01-21'])
+  },
 ]
 
 @Injectable({
   providedIn: 'root'
 })
 export class MockHabitService {
-  habits: Habit[] = mockHabits;
+  habits: Habit[] = mockHabits.slice(0, 4);
 
   getAll(): Habit[] {
     return this.habits;
